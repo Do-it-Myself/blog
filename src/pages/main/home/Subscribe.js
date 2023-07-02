ffab66501d7e2fbd8cff1feba7a940e02509f04b
Do-it-myself <doitmyself111.engineering@gmail.com>
2023-07-02 23:18:35 +0100

    Unfinished subscription

--- src/pages/main/home/Subscribe.js
+++ src/pages/main/home/Subscribe.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 export default function Subscribe() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    if(name.length === 0){
+      alert("Name has left Blank!");
+    }
+    else if (email.length === 0){
+      alert("Email has left Blank!");
+    }
+    else {
+      const url = "https://kfjrgwgp4a.execute-api.eu-west-2.amazonaws.com/dev"
+      axios.post(url, {action:"subscribe", name:name, email:email}).then((response) => console.log(response).catch((error)=>console.log(error)))
+      setName(""); 
+      setEmail("");
+    }
+  }
+
   return (
     <div className="subscribeFlex">
-      <div style={{position: "relative"}}>
+      <div style={{ position: "relative" }}>
         <div className="subscribeBox">
           <div className="stayTuned">Stay tuned</div>
           <div className="wantToReceiveUpdates">
@@ -14,6 +33,8 @@ export default function Subscribe() {
             <input
               className="inputName"
               placeholder="name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
               style={{ width: "calc(var(--subscribeWideWidth)*0.2)" }}
             ></input>
             <div
@@ -22,10 +43,12 @@ export default function Subscribe() {
             <input
               className="inputEmail"
               placeholder="example@gmail.com"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               style={{ width: "calc(var(--subscribeWideWidth)*0.75)" }}
             ></input>
           </div>
-          <button className="subscribeButton" onClick={() => alert("Clicked")}>
+          <button className="subscribeButton" onClick={handleSubmit}>
             Subscribe
           </button>
         </div>
